3435c1312da76f94f569dd0226832744d6943fe1
Holy Paul <brijeeta@gmail.com>
2021-12-23 17:19:50 -0500

    added serviceworker.js, updated index.html,added favicon, modified server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
+// updated port
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// updated heroku connection
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
